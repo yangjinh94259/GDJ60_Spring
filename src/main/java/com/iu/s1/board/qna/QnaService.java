ad8f5f45b4d2f233a71ac6e65c2a2b696484ac1b
yangjinh94259 <yangjinh94259@gmail.com>
2023-02-23 20:35:13 +0900

    Board Ex 23/02/23

--- src/main/java/com/iu/s1/board/qna/QnaService.java
+++ src/main/java/com/iu/s1/board/qna/QnaService.java
@@ -2,33 +2,28 @@ package com.iu.s1.board.qna;
 
 import java.util.List;
 
-import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
+import com.iu.s1.board.BbsDAO;
 import com.iu.s1.board.BbsDTO;
-import com.iu.s1.board.BoardDAO;
 import com.iu.s1.board.BoardDTO;
+import com.iu.s1.board.BoardService;
 import com.iu.s1.util.Pager;
 
-@Repository
-public class QnaDAO implements BoardDAO {
+@Service
+public class QnaService implements BoardService{
 
 	@Autowired
-	private SqlSession sqlSession;
-	
-	private final String NAMESPACE = "com.iu.s1.board.qna.QnaDAO.";
-	
-	@Override
-	public Long getTotalCount(Pager pager) throws Exception {
-		// TODO Auto-generated method stub
-		return sqlSession.selectOne(NAMESPACE + "getTotalCount", pager);
-	}
+	private BbsDAO qnaDAO;
 	
 	@Override
 	public List<BbsDTO> getBoardList(Pager pager) throws Exception {
-		// TODO Auto-generated method stub
-		return sqlSession.selectList(NAMESPACE + "getBoardList", pager);
+		pager.makeRow();
+		
+		pager.makeNum(qnaDAO.getTotalCount(pager));
+		
+		return qnaDAO.getBoardList(pager);
 	}
 
 	@Override
@@ -55,4 +50,6 @@ public class QnaDAO implements BoardDAO {
 		return null;
 	}
 
+	
+	
 }
