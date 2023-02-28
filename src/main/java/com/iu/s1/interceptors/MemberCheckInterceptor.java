81eb75d60c8d482385b1347935853449f579d7c8
yangjinh94259 <yangjinh94259@gmail.com>
2023-02-28 17:38:07 +0900

    Role Ex 23/02/28

--- src/main/java/com/iu/s1/interceptors/MemberCheckInterceptor.java
+++ src/main/java/com/iu/s1/interceptors/MemberCheckInterceptor.java
@@ -18,23 +18,9 @@ public class MemberCheckInterceptor extends HandlerInterceptorAdapter {
 		//return 이 true라면 다음 Controller로 진행
 		//return 이 false라면 다음 Controller로 진행 X
 		System.out.println("Controller 진입 전 체크");
+		
+		response.sendRedirect("");
 		return true;
-	}
-	
-	@Override
-	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-			ModelAndView modelAndView) throws Exception {
-		// TODO Auto-generated method stub
-		//Controller에서 리턴 후 DS 전
-		System.out.println("Controller에서 리턴 후 DS 진입 전");
-	}
-	
-	@Override
-	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
-			throws Exception {
-		// TODO Auto-generated method stub
-		//JSP 렌더링 후 
-		System.out.println("JSP 렌더링 후");;
 	}
 	
 }
